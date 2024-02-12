0902b49304a8afb0b9ecc0c62ee1ba07ddb14ea7
VAdam02 <adam.vilanyi2@gmail.com>
2024-02-13 00:37:16 +0100

    Seeder
    php artisan tinker
    php artisan migrate:fresh --seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Person;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Person::factory()->create();
+        for ($i = 0; $i < 10; $i++) {
+            Person::factory()->create();
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            Person::factory()->create([
+                'name' => 'name' . $i,
+                'email' => 'email' . $i . '@example.com',
+                'age' => 20 + $i,
+                'phone' => '000-0000-000' . $i,
+            ]);
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
