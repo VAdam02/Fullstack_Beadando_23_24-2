5d5db800588b31ca9529ad1c18e0b97b58a3ad39
VAdam02 <adam.vilanyi2@gmail.com>
2024-03-05 13:10:47 +0100

    basic display

--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -1,11 +1,8 @@
 @foreach ($posts as $post)
-    <h2><a href={{ route('posts.show', ["post" => $post->id]) }}>{{ $post->title }}</a></h2>
-    <p>{{ $post->content }}</p>
-    <p><a href={{ route('posts.edit', ["post" => $post->id]) }}>Szerkesztés</a></p>
-    <form action={{ route('posts.destroy', ["post" => $post->id]) }} method="post">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Törlés</button>
-    </form>
-    <hr>
+    <a href="{{ route('posts.show', $post->id) }}">
+        <h2>{{ $post->title }}</h2>
+        <div>
+            {{ $post->content }}
+        </div>
+    </a>
 @endforeach
