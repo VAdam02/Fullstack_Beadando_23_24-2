8b85c13990edb3df506c3fb3957e0b5d994ebbe9
VAdam02 <adam.vilanyi2@gmail.com>
2024-03-07 17:18:01 +0100

    Carousel finished

--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -1,13 +1,13 @@
 @vite(['resources/css/app.css','resources/js/app.js'])
 
 <!-- Header -->
-<header class="bg-gray-800 text-white py-5 px-5">
+<header class="sticky top-0 z-40 bg-gray-800 text-white py-5 px-5">
     <div class="mx-auto">
-        <h1 class="text-2xl font-semibold">Blog - Posts</h1>
+        <h1 class="text-3xl font-semibold">Blog - Posts</h1>
     </div>
 </header>
 
-<div class="relative w-full" data-carousel="slide">
+<div class="w-full" data-carousel="slide">
     <!-- Carousel wrapper -->
     <div class="m-3 relative h-56 overflow-hidden rounded-lg">
         @foreach ($highlightposts as $post)
@@ -15,8 +15,8 @@
             <div class="rounded-lg p-5 bg-gray-700 shadow-md">
                 <a href="{{ route('posts.show', $post->id) }}" class="text-gray-300 hover:text-gray-500 transition duration-500 ease-in-out">
                     <h2 class="truncate font-semibold text-lg mb-2">{{ $post->title }}</h2>
-                    <div class="h-24 overflow-hidden relative">
-                        <div class="absolute bottom-0 left-0 w-full h-10 bg-gradient-to-t from-gray-700 to-transparent"></div>
+                    <div class="h-24 text-justify overflow-hidden relative">
+                        <div class="absolute text-justify bottom-0 left-0 w-full h-10 bg-gradient-to-t from-gray-700 to-transparent"></div>
                         {{ $post->content }}
                     </div>
                 </a>
@@ -30,41 +30,43 @@
         </div>
 
         @endforeach
+        <!-- Slider indicators -->
+        <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+            @foreach ($highlightposts as $post)
+                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="{{ $post->title }}" data-carousel-slide-to="{{ $loop->index }}"></button>
+            @endforeach
+        </div>
+        <!-- Slider controls -->
+        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                </svg>
+                <span class="sr-only">Previous</span>
+            </span>
+        </button>
+        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                </svg>
+                <span class="sr-only">Next</span>
+            </span>
+        </button>
     </div>
-    <!-- Slider indicators -->
-    <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        @foreach ($highlightposts as $post)
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="{{ $post->title }}" data-carousel-slide-to="{{ $loop->index }}"></button>
-        @endforeach
-    </div>
-    <!-- Slider controls -->
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
 </div>
 
 <!-- Content -->
 <div class="mx-5">
+    <h1 class="font-semibold mt-5 mb-3 text-3xl
+    ">Latest posts</h1>
     <!-- List of Posts -->
-    <div class="my-3 gap-4 flex flex-wrap">
+    <div class="my-3 gap-10 flex flex-wrap">
         @foreach ($posts as $post)
-        <div class="flex-auto w-96 rounded-lg p-4 bg-gray-100 shadow-md hover:shadow-lg hover:scale-105 transform transition duration-300 ease-in-out">
+        <div class="sm:w-96 w-full grow rounded-lg p-4 bg-gray-100 shadow-md hover:shadow-lg hover:scale-105 transform transition duration-300 ease-in-out">
             <a href="{{ route('posts.show', $post->id) }}" class="hover:text-gray-500">
                 <h2 class="truncate font-semibold text-lg mb-2">{{ $post->title }}</h2>
-                <div class="h-24 overflow-hidden relative">
+                <div class="h-48 text-justify overflow-hidden relative">
                     <div class="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-gray-100 to-transparent"></div>
                     {{ $post->content }}
                 </div>
